747dd9bec0fe781b11a7a93691b3a0105528055f
lionel deng <babenshin@gmail.com>
2024-08-24 11:51:24 +0800

    points to new git username

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,7 +56,7 @@ export const socials = [
   {
     id: "github",
     icon: github,
-    link: "https://github.com/magnumdatura",
+    link: "https://github.com/nellingDev",
   },
   {
     id: "leetcode",
